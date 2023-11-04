6971c37a8d25e9951595b23ba9ca813d4b5b1552
Erik Umble <erikumble@gmail.com>
2023-11-04 18:45:20 -0400

    initial folder display component

---
+++ AuthBear/components/folderDisplay.vue
@@ -0,0 +1,36 @@
+<script setup>
+import draggable from 'vuedraggable'
+
+const props = defineProps({
+  folders : Array,
+})
+
+function onMoved(event) {
+  console.log("moved", event);
+}
+
+onMounted(() => {
+    console.log("here");
+    console.log(props.folders[0][0]);
+})
+
+</script>
+
+<template>
+    <div>
+      <div v-for="(folder, index) in folders" :key="index">
+        <h3>Folder {{ index + 1 }}</h3>
+
+        <draggable
+            :list="folder" 
+            group="index" 
+            @end="onMoved" 
+            item-key="accountName">
+            <template #item="{element}">
+                <TotpCard :accountName="element.accountName" :code="element.code"></TotpCard>
+            </template>
+        </draggable>
+      </div>
+    </div>
+</template>
+  
