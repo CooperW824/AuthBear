426dc408fe046e1eb905fc60a2b8d38db4afe12c
Cooper Werner <cooperwerner824@gmail.com>
2023-11-04 12:23:41 -0400

    Adding Daisy UI

--- AuthBear/tailwind.config.js
+++ AuthBear/tailwind.config.js
@@ -11,5 +11,22 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#4169e1",
+          secondary: "#6e5393",
+          accent: "#617dff",
+          neutral: "#374151",
+          "base-100": "#374151",
+          info: "#a77edf",
+          success: "#36d399",
+          warning: "#fbbd23",
+          error: "#f87272",
+        },
+      },
+    ],
+  },
 };
